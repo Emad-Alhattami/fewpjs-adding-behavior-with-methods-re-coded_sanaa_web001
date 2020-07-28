6b83f1eeee4a41721a354468768721f29f021705
Emad Alhattami <emad774373827@gmail.com>
2020-07-28 22:06:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 // Your code here
 class Cat {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex =  sex;
+    
+  }
 
+  speak() {
+    return `${this.name} says meow!`
+  }
+}
+
+class Dog {
   constructor(name, sex) {
     this.name = name;
     this.sex =  sex;
   }
 
   speak() {
-    return `${this.name} says meow!`
+    return `${this.name} says woof!`
   }
+}
 
-  class Dog {
-    constructor(name, sex) {
-      this.name = name;
-      this.sex =  sex;
-    }
+class Bird {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex =  sex;
   }
 
 }
